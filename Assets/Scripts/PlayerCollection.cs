92b810baa82c25e586350df36bb8dc7996336f37
Adam Podoxin <adam.podoxin@gmail.com>
2020-03-28 00:01:13 -0700

    Added dash and double jump powerups; implemented gravity flip collection

--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -6,12 +6,38 @@ public class PlayerCollection : MonoBehaviour
 {
     public int amountOfCrystals = 0;
 
+    private PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Crystal"))
+        if (collision.CompareTag("Crystal"))
         {
             Destroy(collision.gameObject);
             amountOfCrystals++;
+        }
+        else if (collision.CompareTag("Powerup"))
+        {
+            Powerup powerup = collision.GetComponent<Powerup>();
+            Destroy(collision.gameObject);
+
+            switch (powerup.powerupType)
+            {
+                case GlobalEnums.PowerupType.Dash:
+                    playerMovement.hasDashAbility = true;
+                    break;
+                case GlobalEnums.PowerupType.DoubleJump:
+                    playerMovement.maxAirJumps = 1;
+                    break;
+                case GlobalEnums.PowerupType.GravityFlip:
+                    //Gravity flip code
+                    print("Gravity flip");
+                    break;
+            }
         }
     }
 }
