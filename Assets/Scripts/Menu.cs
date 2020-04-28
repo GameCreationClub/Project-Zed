b925460f96f4075333f02cbbbcc2da88c89e0cd3
Adam Podoxin <adam.podoxin@gmail.com>
2020-04-27 20:44:49 -0700

    Created basic menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject levelSelection;
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        levelSelection.SetActive(true);
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+}
